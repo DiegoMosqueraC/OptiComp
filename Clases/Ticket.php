547b5bdb241f6bc3d63ce2c810228963d0409112
DanielSieg456 <julianerazo566@gmail.com>
2026-04-06 17:58:35 -0500

    Create Ticket.php

---
+++ Clases/Ticket.php
@@ -0,0 +1,26 @@
+<?php
+//Actividad 6 arquitectura diseño
+class Ticket {
+
+    private $id;
+    private $cliente_id;
+    private $equipo;
+    private $descripcion;
+    private $estado;
+    private $fecha_ingreso;
+
+    public function __construct($cliente_id, $equipo, $descripcion, $estado, $fecha_ingreso) {
+        $this->cliente_id = $cliente_id;
+        $this->equipo = $equipo;
+        $this->descripcion = $descripcion;
+        $this->estado = $estado;
+        $this->fecha_ingreso = $fecha_ingreso;
+    }
+
+    public function getClienteId(){ return $this->cliente_id; }
+    public function getEquipo(){ return $this->equipo; }
+    public function getDescripcion(){ return $this->descripcion; }
+    public function getEstado(){ return $this->estado; }
+    public function getFechaIngreso(){ return $this->fecha_ingreso; }
+}
+?>
