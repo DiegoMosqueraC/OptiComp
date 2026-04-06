2a61fbf538a0a4522d74309af83ecc873270db11
DanielSieg456 <julianerazo566@gmail.com>
2026-04-06 17:49:02 -0500

    Create index6.php

---
+++ public/index6.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once "app/Core/Ticket.php";
+require_once "app/Data/TicketDAO.php";
+
+$ticket = new Ticket(
+    1,
+    "Laptop ASUS",
+    "No enciende",
+    "Pendiente",
+    date("Y-m-d")
+);
+
+$dao = new TicketDAO();
+
+$resultado = $dao->crear($ticket);
+
+echo "<h2>Resultado:</h2>";
+echo $resultado;
