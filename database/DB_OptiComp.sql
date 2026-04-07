66f2b4e8df5108d672b8001eed5a552755c491a9
DanielSieg456 <julianerazo566@gmail.com>
2026-04-07 09:58:34 -0500

    Create DB_OptiComp.sql

---
+++ database/DB_OptiComp.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE DB_OptiComp;
+USE DB_OptiComp;
+
+CREATE TABLE tickets (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente_id INT,
+    equipo VARCHAR(100),
+    descripcion TEXT,
+    estado VARCHAR(50),
+    fecha_ingreso DATE
+);
