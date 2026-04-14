ad53e4fa0aa9059f4d1c74b6eae1d5545cc808aa
DanielSieg456 <julianerazo566@gmail.com>
2026-04-13 20:24:37 -0500

    Update Ticket.php

--- Clases/Ticket.php
+++ Clases/Ticket.php
@@ -1,13 +1,13 @@
 <?php
 //Actividad 6 arquitectura diseño
 class Ticket {
-    
-    public $id;
-    public $cliente_id;
-    public $equipo;
-    public $descripcion;
-    public $estado;
-    public $fecha_ingreso;
+    private $id;
+    private $cliente_id;
+    private $equipo;
+    private $descripcion;
+    private $estado;
+    private $fecha_ingreso;
+    private $fecha_salida;
 
     public function __construct($cliente_id, $equipo, $descripcion, $estado, $fecha_ingreso) {
         $this->cliente_id = $cliente_id;
@@ -17,10 +17,22 @@ class Ticket {
         $this->fecha_ingreso = $fecha_ingreso;
     }
 
-    public function getClienteId(){ return $this->cliente_id; }
-    public function getEquipo(){ return $this->equipo; }
-    public function getDescripcion(){ return $this->descripcion; }
-    public function getEstado(){ return $this->estado; }
-    public function getFechaIngreso(){ return $this->fecha_ingreso; }
+
+    public function getId() { return $this->id; }
+    public function getClienteId() { return $this->cliente_id; }
+    public function getEquipo() { return $this->equipo; }
+    public function getDescripcion() { return $this->descripcion; }
+    public function getEstado() { return $this->estado; }
+    public function getFechaIngreso() { return $this->fecha_ingreso; }
+    public function getFechaSalida() { return $this->fecha_salida; }
+
+
+    public function setId($id) { $this->id = $id; }
+    public function setClienteId($cliente_id) { $this->cliente_id = $cliente_id; }
+    public function setEquipo($equipo) { $this->equipo = $equipo; }
+    public function setDescripcion($descripcion) { $this->descripcion = $descripcion; }
+    public function setEstado($estado) { $this->estado = $estado; }
+    public function setFechaIngreso($fecha_ingreso) { $this->fecha_ingreso = $fecha_ingreso; }
+    public function setFechaSalida($fecha_salida) { $this->fecha_salida = $fecha_salida; }
 }
 ?>
